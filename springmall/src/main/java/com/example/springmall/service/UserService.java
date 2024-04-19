a0da8b391eb27efb3fb9746babd3d17e86ba0db6
LAPTOP-B9CE46NT\frank <frank.lu@frog-jump.com>
2024-04-19 12:27:30 +0800

    帳號功能 - 使用MD5產生密碼雜湊值

--- springmall/src/main/java/com/example/springmall/service/UserService.java
+++ springmall/src/main/java/com/example/springmall/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.springmall.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import com.example.springmall.dao.UserDao;
 import com.example.springmall.dto.UserLoginRequest;
@@ -15,6 +16,9 @@ public class UserService {
 	private UserDao userDao;
 
 	public UserVO register(UserRegisterRequest request) {
+		
+		// 使用 MD5 生成密碼的雜湊值
+		request.setPassword(makeMD5(request.getPassword().getBytes()));
 
 		Integer userId = userDao.createUser(request);
 
@@ -32,7 +36,10 @@ public class UserService {
 			return null;
 		}
 		
-		if(!request.getPassword().equals(user.getPassword())){
+		// 使用 MD5 生成密碼的雜湊值
+		String hashPassword = makeMD5(request.getPassword().getBytes());
+		
+		if(!hashPassword.equals(user.getPassword())){
 			return null;
 		}
 		
@@ -45,6 +52,10 @@ public class UserService {
 
 	public UserVO getUserByEmail(String email) {
 		return userDao.getUserByEmail(email);
+	}
+	
+	private String makeMD5(byte[] bytes) {
+		return DigestUtils.md5DigestAsHex(bytes);
 	}
 
 }
